2cbd4c10280f153de7d3519ee5c24e1ae85ea87a
ssfrs <ovidiu.alexandru.iftemie.gmail.com>
2021-06-09 19:48:57 +0300

    patch si delete

--- index.js
+++ index.js
@@ -25,7 +25,42 @@ function displayTodos(todos) {
         const li = document.createElement('li');
         li.textContent = todo.description;
         li.classList = todo.done ? "done" : '';
+
+        const span = document.createElement('span');
+        span.textContent = 'X';
+        li.appendChild(span);
+
         todoContainer.appendChild(li);
+
+        li.addEventListener('click', function() {
+            fetch(`http://localhost:3000/api/todos/${todo.id}`, {
+                method: 'PATCH',
+                headers: {
+                    Authorization: `Bearer ${localStorage.getItem('token')}`,
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({
+                    description: todo.description,
+                    done: !todo.done
+                })
+            })
+            .then(response => response.json())
+            .then(() => getTodos())
+        });
+// ---------------------
+        span.addEventListener('click', function(e) {
+            e.stopPropagation();
+            fetch(`http://localhost:3000/api/todos/${todo.id}`, {
+                method: 'DELETE',
+                headers: {
+                    Authorization: `Bearer ${localStorage.getItem('token')}`,
+                    'Content-Type': 'application/json'
+                },
+            })
+            .then(response => response.json())
+            .then(() => getTodos())
+        });
+// ---------------------
     }
 }
 
